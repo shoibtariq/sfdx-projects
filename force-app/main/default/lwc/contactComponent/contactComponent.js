89ca8f37f843974b4334a00ca4ba58bce6536b64
shoibtariq <shoibjooo@gmail.com>
2019-07-26 14:31:23 +0530

    hello

--- force-app/main/default/lwc/contactComponent/contactComponent.js
+++ force-app/main/default/lwc/contactComponent/contactComponent.js
@@ -1,23 +1,29 @@
-import { LightningElement,track,wire } from 'lwc';
+import {
+    LightningElement,
+    track,
+    wire
+} from 'lwc';
 import getContactList from '@salesforce/apex/AccountHandler.getContactList';
 const DELAY = 300;
 export default class ContactComponent extends LightningElement {
 
-@track searchKey ='';
+    @track searchKey = '';
 
-@wire(getContactList,{searchKey:'$searchKey'}) contacts;
+    @wire(getContactList, {
+        searchKey: '$searchKey'
+    }) contacts;
 
 
-handleKeyChange(event) {
-    // Debouncing this method: Do not update the reactive property as long as this function is
-    // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
+    handleKeyChange(event) {
+        // Debouncing this method: Do not update the reactive property as long as this function is
+        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
 
-    window.clearTimeout(this.delayTimeout);
-    const searchKey = event.target.value;
-    /* eslint-disable  */
-    this.delayTimeout = setTimeout(() => {
-        this.searchKey = searchKey;
-    }, DELAY);
-}
+        window.clearTimeout(this.delayTimeout);
+        const searchKey = event.target.value;
+        /* eslint-disable  */
+        this.delayTimeout = setTimeout(() => {
+            this.searchKey = searchKey;
+        }, DELAY);
+    }
 
 }
---
+++ force-app/main/default/lwc/viewTreeSelected/viewTreeSelected.js
@@ -0,0 +1,8 @@
+import {
+    LightningElement,
+    api
+} from 'lwc';
+
+export default class ViewTreeSelected extends LightningElement {
+    @api recordid;
+}
